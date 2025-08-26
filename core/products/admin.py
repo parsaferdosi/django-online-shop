64330ed75b19cc654d1fa3a5559665fd61133b9e
Elmira <elmira.nikrioodi96@gmail.com>
2025-08-26 23:16:23 +0330

    added comment + like model

--- core/products/admin.py
+++ core/products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Product , Category , Comment , Like
 # Register your models here.
+
+
+admin.site.register(Category)
+
+admin.site.register(Like)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title","status","average_stars"]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["product","user","like_count","dislike_count"]
+
+
--- core/products/models.py
+++ core/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxLengthValidator , MinValueValidator
+from django.core.validators import MaxValueValidator , MinValueValidator
+from django.db.models import Avg
 # Create your models here.
 
 User =get_user_model()
@@ -26,16 +27,53 @@ class Product(models.Model):
     slug = models.SlugField(unique= True)
     image = models.ImageField(upload_to='products/%Y/%m/%d' , null= True , blank=True)
     description = models.TextField()
-    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category , related_name="cat_products")
     price = models.PositiveIntegerField(default=0)
-    stars = models.PositiveIntegerField(default=0 , validators=[MinValueValidator(0),MaxLengthValidator(5)])
     status = models.CharField(choices=STATUS_CHOICE , max_length=100)
     quantity = models.PositiveIntegerField(default=0)
     seller = models.ForeignKey(User , on_delete=models.CASCADE , related_name="user_products")
     created_at = models.DateTimeField(auto_now_add= True)
     updated = models.DateTimeField(auto_now=True)
 
+    def average_stars(self):
+        result = self.comments.aggregate(avg=Avg("stars"))["avg"]
+        return result or 0
+    
     def __str__(self):
         return self.title
-
     
+
+class Comment(models.Model):
+
+    STATUS_CHOICES = [
+        ('approved', 'تأیید شده'),
+        ('pending', 'در انتظار تأیید'),
+    ]
+
+
+    product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name='comments')
+    user = models.ForeignKey(User , on_delete=models.CASCADE )
+    stars = models.PositiveIntegerField(default=0 , validators=[MinValueValidator(0),MaxValueValidator(5)])
+    text = models.TextField()
+    status = models.CharField(choices=STATUS_CHOICES , default='pending',max_length=28)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.product}"
+    
+    def like_count(self):
+        return self.likes.filter(is_like = True).count()
+    
+    def dislike_count(self):
+        return self.likes.filter(is_like = False).count()
+
+
+
+class Like(models.Model):
+
+    comment = models.ForeignKey(Comment , on_delete=models.CASCADE , related_name="likes")
+    user = models.ForeignKey(User , on_delete=models.CASCADE )
+    is_like = models.BooleanField() # False = dislike , True = like
+
+    class Meta :
+        unique_together = ("user","comment") #User can be just have one like 
