ab77b88214e39e43c5045a0bf4b907e923cec464
parsa <parsa78963210@gmail.com>
2025-09-29 17:25:17 +0330

    feat:searchAPI in product app

--- core/core/settings.py
+++ core/core/settings.py
@@ -155,6 +155,11 @@ REST_FRAMEWORK = {
         
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS':[
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE':10,
